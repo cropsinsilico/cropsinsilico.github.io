339c02e7e717ce8e6863473efa61363c09276597
Meagan Lang <langmm.astro@gmail.com>
2019-02-02 13:50:48 -0600

    rebuilt docs

--- cis_interface/static/documentation_options.js
+++ cis_interface/static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.6.1+424.ga1da56a3',
+    VERSION: '0.7.4+1.g8a6e5f7c.dirty',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
